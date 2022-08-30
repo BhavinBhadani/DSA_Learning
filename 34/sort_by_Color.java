6ba089cea15c695558f3b0d1d197602d648ee241
Bhavin <bhavinbhadani@yahoo.in>
2022-08-30 14:48:18 +0530

    Problem desc and solution

---
+++ 34/sort_by_Color.java
@@ -0,0 +1,71 @@
+/*    
+
+Q. Sort by Color
+
+Problem Description
+
+Given an array with N objects colored red, white, or blue, sort them so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+We will use the integers 0, 1, and 2 to represent red, white, and blue, respectively.
+
+Note: Using the library sort function is not allowed.
+
+
+Problem Constraints
+1 <= N <= 1000000
+0 <= A[i] <= 2
+
+
+Input Format
+First and only argument of input contains an integer array A.
+
+Output Format
+Return an integer array in asked order
+
+
+Example Input
+Input 1 :
+    A = [0 1 2 0 1 2]
+
+Input 2:
+    A = [0]
+
+
+
+Example Output
+Output 1:
+    [0 0 1 1 2 2]
+
+Output 2:
+    [0]
+
+
+Example Explanation
+Explanation 1:
+    [0 0 1 1 2 2] is the required order.
+
+*/
+   public ArrayList<Integer> sortColors(ArrayList<Integer> A) {
+        int n = A.size();
+        int pos0 = 0;
+        int pos1 = n - 1;
+        int pos2 = n - 1;
+        while (pos0 < pos2) {
+            if (A.get(pos0) == 0)
+                pos0++;
+            else if (A.get(pos0) == 1) {
+                if (pos0 < pos1) {
+                    Collections.swap(A, pos0, pos1);
+                    pos1--;
+                }
+                else
+                    pos0++;
+            }
+            else {
+                Collections.swap(A, pos0, pos2);
+                pos2--;
+                if (pos1 > pos2)
+                    pos1 = pos2;
+            }
+        }
+        return A;
+    }
