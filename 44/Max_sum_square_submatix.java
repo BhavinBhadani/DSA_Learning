57768f71c1645ea046b262962c42465c1631425d
Bhavin <bhavinbhadani@yahoo.in>
2022-10-01 17:50:56 +0530

    updated with better solution

--- 44/Max_sum_square_submatix.java
+++ 44/Max_sum_square_submatix.java
@@ -72,3 +72,34 @@ public class Solution {
         return maxSum;
     }
 }
+
+//--- Solution with DP (Better solution) ---
+public class Solution {
+    public int solve(int[][] A, int B) {
+        int row = A.length;
+        int col = A[0].length;
+        int maxSum = Integer.MIN_VALUE;
+        int[][] dp = new int[row+1][col+1];
+
+        // sum
+        for(int i=1; i<dp.length; i++) {
+            for(int j=1; j<dp[0].length; j++) {
+                dp[i][j] = A[i-1][j-1] + dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1];
+            }
+        }
+
+        // process
+        for(int i=1; i<dp.length; i++) {
+            for(int j=1; j<dp[0].length; j++) {
+                // B*B
+                int sum = Integer.MIN_VALUE;
+                if(i-B>=0 && j-B>=0) {
+                    sum = dp[i][j] - dp[i-B][j] - dp[i][j-B] + dp[i-B][j-B];
+                    maxSum = Math.max(sum, maxSum);
+                }
+            }
+        }
+        return maxSum;
+    }
+}
+
